541159acd4cb3b75281211240aca06eb64252e09
Rao Aaraf Younus <aarafrao22@gmail.com>
2022-04-14 21:11:44 +0500

    v1

--- app/src/main/java/com/aarafrao/mvvm/MainActivity.kt
+++ app/src/main/java/com/aarafrao/mvvm/MainActivity.kt
@@ -2,14 +2,28 @@ package com.aarafrao.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
 import com.aarafrao.mvvm.UserViewModel.UserViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var userViewModel: UserViewModel
+    private lateinit var builder: AlertDialog.Builder
+    private lateinit var alertDialog: AlertDialog
+    private lateinit var edName: EditText
+    private lateinit var edAge: EditText
+    private lateinit var btnSave: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +33,36 @@ class MainActivity : AppCompatActivity() {
         userViewModel.getAllUserData(applicationContext)?.observe(this, Observer {
 
         })
+
+        floatingActionButton.setOnClickListener {
+            openDialog()
+        }
+    }
+
+    private fun openDialog() {
+        builder = AlertDialog.Builder(this)
+        var itemView: View = LayoutInflater.from(applicationContext).inflate(R.layout.dialog, null)
+        alertDialog = builder.create()
+        alertDialog.setView(itemView)
+        edAge = itemView.findViewById(R.id.edAge)
+        edName = itemView.findViewById(R.id.edName)
+        btnSave = itemView.findViewById(R.id.btnSave)
+
+        btnSave.setOnClickListener {
+            storeDataIntoRoom()
+        }
+        alertDialog.show()
+    }
+
+    private fun storeDataIntoRoom() {
+        val getName = edName.text.toString().trim()
+        val getAge = edAge.text.toString().trim()
+
+        if (!TextUtils.isEmpty(getName) && !TextUtils.isEmpty(getAge)) {
+
+        } else {
+            Toast.makeText(this, "Enter Something first", Toast.LENGTH_LONG).show()
+            alertDialog.dismiss()
+        }
     }
 }
